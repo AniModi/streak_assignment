cde99ae584f1ab3738d127d54d42f0902f11daf5
AniModi <98632268+AniModi@users.noreply.github.com>
2024-06-17 21:57:19 +0530

    updated env and index file

--- src/service/avInstance.ts
+++ src/service/avInstance.ts
@@ -1,9 +1,8 @@
 import axios from "axios";
 
-const API_KEY = import.meta.env.VITE_API_KEY;
+const API_KEY = import.meta.env.VITE_ALPHA_VANTAGE_API_KEY;
 
 const ALPHA_VANTAGE_BASE_URL = "https://www.alphavantage.co/query";
-
 
 const avInstance = axios.create({
   baseURL: ALPHA_VANTAGE_BASE_URL,
